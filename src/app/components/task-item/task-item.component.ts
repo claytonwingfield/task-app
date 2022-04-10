91b1726327e02836de9999d39ba64900076dd128
claytonwingfield <clayton.wingfield@okstate.edu>
2022-04-10 16:19:34 -0500

    icon

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../../Task';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -9,8 +10,11 @@ import { Task } from '../../Task';
 export class TaskItemComponent implements OnInit {
 
   @Input() task!: Task;
+  faTimes = faTimes;
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
   }
